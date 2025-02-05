b1a7de927c495eeaa8852b7658413d5ae364f7fc
Melvin Sjöstrand <melvin.sjostrand12@gmail.com>
2025-02-05 16:31:23 +0100

    Update
    nav done
    Hero done
    Highlights working on it

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,8 +13,8 @@ const Hero = () => {
         }
     }
     useGSAP(() => {
-    gsap.to('#hero', {opacity:1, delay:1.5})
-    gsap.to('#cta', {opacity:1, y:-50,delay:1.5})
+    gsap.to('#hero', {opacity:1, delay:2})
+    gsap.to('#cta', {opacity:1, y:-50,delay:2})
   }, [])
 
   useEffect(() => {
@@ -33,9 +33,10 @@ const Hero = () => {
                 </video>
             </div>
         </div>
-        <div id="cta" className="flex flex-col item-center opacity-0 translate-y-20">
-            <a href="#highlights" className="btn">Buy</a>
-            <p className="font-normal text-xl">From $199/month or $999</p>
+        <div id="cta" 
+            className="flex flex-col items-center opacity-0 translate-y-20">
+                <a href="#highlights" className="btn">Buy</a>
+                <p className="font-normal text-xl">From $199/month or $999</p>
         </div>
     </section>
   )
